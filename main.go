28c932ec32e83bd875efd40c788c6bdc52928d8c
Aliaksandr Mukhin <aliaksandr.mukhin@coxautoinc.com>
2024-05-18 18:46:07 +0400

    simplified counter

--- main.go
+++ main.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	isLine := flag.Bool("l", false, "count lines")
-	flag.Parse()
-	fmt.Println(Count(os.Stdin, *isLine))
+	fmt.Println(Count(os.Stdin))
 }
 
-func Count(r io.Reader, isLine bool) int {
+func Count(r io.Reader) int {
 	scanner := bufio.NewScanner(r)
-	if !isLine {
-		scanner.Split(bufio.ScanWords)
-	}
+	scanner.Split(bufio.ScanWords)
 	i := 0
 	for scanner.Scan() {
 		i++
